bcdcbe8360a891479d0211340257480a4a99e80d
Cristina Mariscal <cristina.mariscal.tx@gmail.com>
2026-06-09 14:10:16 -0400

    Se añadio la leccion de training para Saucedemo

---
+++ tests/training.spec.ts
@@ -0,0 +1,10 @@
+import {test, expect} from '@playwright/test'
+// page = fixture 
+// await es una funcion que dice , espere antes que la otra funcion se jecute
+test('Login Sauce demo', async ({page}) => {
+    await page.goto('https://saucedemo.com')
+
+    await page.getByRole('textbox', {name: 'Username'}).fill('standard_user')
+    await page.getByRole('textbox', {name:'Password'}).fill('secret_sauce')
+    await page.getByRole('button', {name: 'Login'}).click()
+})
