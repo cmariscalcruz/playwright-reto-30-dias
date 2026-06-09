cb01f8000f9bf612d98e25f96bcc5f8587c7e92d
Cristina Mariscal <cristina.mariscal.tx@gmail.com>
2026-06-09 15:51:26 -0400

    Reto1 completado  login to OrangeHRM

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,6 +30,11 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+
+    // hace que mi test sea mas lent
+   /* launchOptions: {
+      slowMo: 10000
+    } */
   },
 
   /* Configure projects for major browsers */
