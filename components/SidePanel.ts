78afcaeb92127f80f4d953d3fe3ce9d3e8167549
Cristina Mariscal <cristina.mariscal.tx@gmail.com>
2026-06-30 22:15:05 -0400

    added SidePanel test (reto 8)

--- components/SidePanel.ts
+++ components/SidePanel.ts
@@ -1,10 +1,12 @@
-import {Page, Locator} from "@playwright/test"
+import {Page, Locator, expect} from "@playwright/test"
 
 export class SidePanel {
      readonly page : Page
+     readonly searchtextbox : Locator
 
      constructor (page: Page){
         this.page = page
+        this.searchtextbox = page.getByRole('textbox', {name:'Search'})
      }
 
      //CREATE A PRIVATE METHO TO LOCATOR DINAMICALLY
@@ -14,6 +16,15 @@ export class SidePanel {
     async clickOnOption (option : SideMenuOption) {
         await this.menuOption(option).click()
     }
+
+    async searchOption (option : SideMenuOption){
+        await this.searchtextbox.fill(option)
+        expect(this.searchtextbox).toHaveValue(option)
+        console.log(option)
+        //expect(page.locator("//label[contains(.,'Username')]/parent::div/following-sibling::div/input")).toHaveValue(userToEdition)
+    }
+
+
 }
 
 export enum SideMenuOption {
---
+++ tests/sidepanel.spec.ts
@@ -0,0 +1,37 @@
+import{test, expect} from "@playwright/test"
+import { LoginPage } from "../PageObjects/LoginPage"
+import { SideMenuOption, SidePanel } from "../components/SidePanel"
+
+test ("Login with POM and linking in all options from sidePanel", async ({page}) =>{
+    const pageLogin = new LoginPage(page)
+    await pageLogin.doLogin('Admin','admin123')
+
+     // call tu sidePanel
+    const sidePanel = new SidePanel(page)
+    await sidePanel.clickOnOption(SideMenuOption.ADMIN)
+    await sidePanel.clickOnOption(SideMenuOption.PIM)
+    await sidePanel.clickOnOption(SideMenuOption.LEAVE)
+    await sidePanel.clickOnOption(SideMenuOption.TIME)
+    await sidePanel.clickOnOption(SideMenuOption.RECRUITMENT)
+    await sidePanel.clickOnOption(SideMenuOption.MY_INFO)
+    await sidePanel.clickOnOption(SideMenuOption.PERFORMANCE)
+    await sidePanel.clickOnOption(SideMenuOption.DASHBOARD)
+    await sidePanel.clickOnOption(SideMenuOption.DIRECTORY)
+    await sidePanel.clickOnOption(SideMenuOption.CLAIM)
+    await sidePanel.clickOnOption(SideMenuOption.BUZZ)
+    await sidePanel.clickOnOption(SideMenuOption.MAINTENACE)
+})
+
+test("Check 'Search' filters the option", async ({page}) => {
+     const pageLogin = new LoginPage(page)
+    await pageLogin.doLogin('Admin','admin123')
+
+    const searchInput = new SidePanel(page);
+    await searchInput.searchOption(SideMenuOption.ADMIN)
+    await searchInput.searchOption(SideMenuOption.PIM)
+    await searchInput.searchOption(SideMenuOption.LEAVE)
+    await searchInput.searchOption(SideMenuOption.PERFORMANCE)
+    await searchInput.searchOption(SideMenuOption.CLAIM)
+
+   
+})
